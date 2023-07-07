a89bf67d29aa44df1d8841f5f297b9cb82db76fe
[Javier <[sanzalvarezjavier@gmail.com]>
2023-07-07 18:39:05 +0200

    first version home and about

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,27 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
-import Styles from "./styles.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Styles from './index.css'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+ReactDOM
+  .createRoot(document.getElementById('root'))
+  .render(<App />);
--- src/modules/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Footer() {
-    return (
-        <div className="footer">
-            <p className="footer-text">Ⓒ 2022 #VANLIFE</p>
-        </div>
-    );
-}
